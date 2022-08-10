2c897ea217f6229308e019680a493dfdfcc15fe8
Ahkal <bp@sojourn.llc>
2022-08-10 17:13:16 -0400

    - Adds mock data - Connects to Apollo Explorer

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,33 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 
-const server = new ApolloServer({ typeDefs });
+const mocks = {
+  Query: () => ({
+    tracksForHome: () => [...new Array(7)],
+  }),
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
+    author: () => {
+      return {
+        name: "Grumpy Cat",
+        photo:
+          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+      };
+    },
+    thumbnail: () =>
+      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+};
+
+const server = new ApolloServer({ typeDefs, mocks });
 
 server.listen().then(() => {
   console.log(`
     🚀 Server's running!
-    🔊 Listening @ Port: 3000
-    📬 Query @ http://localhost:3000
+    🔊 Listening @ Port: 4000
+    📬 Query @ http://localhost:4000
   `);
 });
