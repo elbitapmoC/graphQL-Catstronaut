680c5990cf4d2347ade878cc63c65c656dbaded3
Ahkal <bp@sojourn.llc>
2022-08-10 15:22:51 -0400

    - Adds listener to serverr

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,12 @@
-//TODO
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+  console.log(`
+    🚀 Server's running!
+    🔊 Listening @ Port: 3000
+    📬 Query @ http://localhost:3000
+  `);
+});
